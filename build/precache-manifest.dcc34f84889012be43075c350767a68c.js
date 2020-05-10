7fe6e7db3a4eb118be6b496336f284b6ce951416
aditya <kumar.93@iitj.ac.in>
2020-05-10 12:25:06 +0530

    ver0: hosting

---
+++ build/precache-manifest.dcc34f84889012be43075c350767a68c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255878afac2b156fcd1c0fd1dac20a87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4f563c23d21566b886e",
+    "url": "/static/css/main.b7889bac.chunk.css"
+  },
+  {
+    "revision": "03ea07821e1cb9f9c6f5",
+    "url": "/static/js/2.f8795d8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8795d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f563c23d21566b886e",
+    "url": "/static/js/main.e7332792.chunk.js"
+  },
+  {
+    "revision": "8307479eff45487f5469",
+    "url": "/static/js/runtime-main.0c728f3b.js"
+  }
+]);
